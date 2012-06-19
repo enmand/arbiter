746423cd7cf58e347a256bbf9c214ae115b713b2
Daniel Enman <enmand@unerror.com>
2012-06-18 23:00:28 -0300

    Prototypes and and macros

--- daemon.h
+++ daemon.h
@@ -1,14 +1,36 @@
+#ifndef _DAEMON_H_
+#define _DAEMON_H_
+
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <signal.h>
 
 #include <fcntl.h>
 #include <unistd.h>
+
+#if __STDC_VERSION__ >= 199901L // c99
+#include <stdbool.h>
+#else
+typedef enum {false=0, true} bool;
+#endif
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define ARBITER_START 1
+#define ARBITER_STOP 0
+#define ARBITER_PFILE "arbiter.pid"
+
 // prototypes for deamon and locking
-void _setpid(pid_t);
+void daemonize(int);
+
+void _setpidf(pid_t);
+pid_t _getpidf(void);
+bool _rmpid();
+
+void _start();
+bool _stop();
 void _handlesig(int);
-void daemonize(void);
+
+#endif
