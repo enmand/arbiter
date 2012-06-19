bf5f06fe0982dacf79d645f1de6763129c03e163
Daniel Enman <enmand@unerror.com>
2012-06-19 15:39:58 -0300

    Use getopts for command line configuation

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
 all: clean .c.so main
 
 main:
-	gcc -Wall -std=c99 -I/opt/local/include -L/opt/local/lib -L. -ldaemon -lzmq -lmongoc arbiter.c -o arbiter
+	gcc -Wall -O3 -std=c99 -I/opt/local/include -L/opt/local/lib -pthread -L. -ldaemon -lzmq -lmongoc arbiter.c -o arbiter
 
-.c.so:
-	gcc -Wall -c -fPIC -o daemon.o daemon.c
+.c.so: daemon
+
+daemon:
+	gcc -Wall -O3 -c -fPIC -o daemon.o daemon.c
 	gcc -shared -Wl,-install_name,libdaemon.so -o libdaemon.so daemon.o
 	rm daemon.o
 
 clean:
-	rm -f *.so
+	rm -f *.so *.o
 	rm -f arbiter
-	rm -f arbiter.pid
--- arbiter.c
+++ arbiter.c
@@ -1,30 +1,39 @@
 
 #include <zmq.h>
 #include <mongo.h>
-#include <stdbool.h>
 
+#include <stdbool.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <getopt.h>
+
 #include "daemon.h"
 
+void parse_opts(int, char**, bool*, char**, int*, char**, char**, int*);
+void usage(const char*);
 
 int main(int argc, char *argv[])
 {
-	if(argc < 2)
+	bool fork;
+	char *action = 0;
+	int port = 1802;
+	char *listen = "*";
+	char *mongo_host = "127.0.0.1";
+	int mongo_port = 27017;
+
+	parse_opts(argc, argv, &fork, &action, &port, &listen, &mongo_host, &mongo_port);
+
+	if(fork)
 	{
-		fprintf(stderr, "Usage:\n%s start|stop\n\n", argv[0]);
-		fprintf(stderr, "\tstart: start execution of the daemon\n");
-		fprintf(stderr, "\tstop:  stop execution of the daemon\n\n");
-		exit(EXIT_FAILURE);
-	}
-	if(strcmp(argv[1], "start") == 0)
-	{
-		daemonize(ARBITER_START);
-	} else 
-	{
-		daemonize(ARBITER_STOP);
+		if(strcmp(action, "start") == 0)
+		{
+			daemonize(ARBITER_START);
+		} else 
+		{
+			daemonize(ARBITER_STOP);
+		}
 	}
 
 	void *zctx  = zmq_init(2);
@@ -53,4 +62,52 @@ int main(int argc, char *argv[])
 
 	mongo_destroy(conn);
 	free(conn);
+}
+
+void parse_opts(int argc, char *argv[], bool *fork, char** action, 
+					int* port, char** listen, char** mongo_host, 
+					int* mongo_port)
+{
+	int opt;
+	bool dostart = false;
+	while((opt = getopt(argc, argv, "sf:p:l:m:h")) != -1)
+	{
+		switch(opt)
+		{
+			case 'h':
+				usage(argv[0]);
+				exit(EXIT_SUCCESS);
+			case 'f':
+				dostart = true;
+				*fork = true;
+				*action = optarg;
+				break;
+			case 's':
+				dostart = true;
+				*fork = false;
+				break;
+			default:
+				usage(argv[0]);
+				exit(EXIT_FAILURE);
+		}
+	}
+	if(!dostart)
+	{
+		fprintf(stderr, "ERROR: -s or -f is required to start\n\n");
+		usage(argv[0]);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void usage(const char *name)
+{
+	fprintf(stderr, "Usage:\n%s [-f start|stop] [-h] \n\n", name);
+	fprintf(stderr, "where:\n");
+	fprintf(stderr, " -h\t\tPrint this help.\n");
+	fprintf(stderr, " -s\t\tDon't fork, and log to stderr\n");
+	fprintf(stderr, " -f start|stop\tCreate a daemon.\n");
+	fprintf(stderr, " -p\t\tPort number. (default: 1802)\n");
+	fprintf(stderr, " -l\t\tListen address. (default: 0.0.0.0)\n");
+	fprintf(stderr, " -m\t\tMongo host to connect to. (default: 127.0.0.1)\n");
+	fprintf(stderr, " -n\t\tMongo port to connect to. (default: 27017)");
 }
