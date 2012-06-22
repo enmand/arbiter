eb83fcda20fb8cac12c791e7d27255ec4affb929
Daniel Enman <enmand@unerror.com>
2012-06-21 22:27:08 -0300

    Add empty logging .c and .h for Makefile

---
+++ src/logging.c
@@ -0,0 +1,6 @@
+#include "logging.h"
+
+void log_add(const char *message)
+{
+	
+}
---
+++ src/logging.h
@@ -0,0 +1,8 @@
+#ifndef _ARBITER_LOGGING_H
+#define _ARBITER_LOGGING_H
+
+#include <stdio.h>
+
+void log_add(const char *);
+
+#endif
